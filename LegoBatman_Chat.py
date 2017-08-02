52d61bb59ee8d91713315edaf58aff1d2832d978
deelisfdc <deelisfdc@gmail.com>
2017-08-02 04:50:42 -0700

    Version3 removed nonworking parts, Turtle not launching

--- LegoBatman_Chat.py
+++ LegoBatman_Chat.py
@@ -1,13 +1,12 @@
-#With each response, Lego Batman acknowleges the answer with, "Cool!" and spins off with adding something about his own favorite color or where he lives.
+#This Q&A game was created to help me and my child learn Python with a favorite subject.
+#Chat introduces lego Batman, Asks for user name and information. With each response, Lego Batman acknowleges the answer with, "Cool!" and spins off with adding something about his own favorite color or where he lives.
 #2. Lego Batman will then go off on a tangent about himself and then says ok, time for the Q&A. Ask me anything you want.
 #3. From here: and a short list of 3 topics comes up. User selects a topic, and three questions pop up. User selects a question for Lego Batman to ask.
 #4. Lego Batman answers, sometimes ending his response by asking the user another question depending on the selection.
 # 5. Depending on the question selected, the Bat Signal will go off and lego batman will have to split and the theme song background is splayed (repeats) across the screen and opens a closing image thanking the user for being a super fan.
-
 #Nice to have: when time is up, Lego Batman cuts off the chat with something snarky and says Time's Up. Bye! and opens an image thanking the super duper fan.
 
-#introduces lego Batman, Askes for user name and information
-
+#Some language, trivia and quotes are borrowed from "The Lego Batman Movie Essential Guide" and "Batman's Guide to Being Cool"
 
 # ideas for pulling random questions for second section
     # #import random
@@ -36,70 +35,92 @@
             #("T!", 0)
             #])
 
+#import random
+# import turtle
+
+# # def open_turtle():
+# #     window = turtle.Screen()
+# #     window.bgcolor("black")
+# #     window.title("Draw Your Logo Here")
+# #     #window.showturtle()
+   
+# window = turtle.Screen()
+
+# window.bgcolor("black")
+
+# window.title("Draw Your Logo Here")
+
+# logo = turtle.Turtle()
+
+
+# def draw_turtle(newturtle):
+
+#     turtle.ondrag(turtle.goto)
+
+#     turtle.listen()
+    
+
+# def drawing_preference():
+#     """Open turtle screen for logo drawing"""
+
+#     logo.shape('classic')
+#     logo.color("green", "yellow")
+#     logo.pencolor("white")
+#     logo.width("10")
+
+
+# def draw():
+#     """draw turtle"""
+
+#     drawing_preference()
+
+#     draw_turtle(logo)
+
+#     window.exitonclick()
+
 
 my_guest_list = []
-# start_choice = 
 
 #guest_name = {'enemy': JOKER, CATWOMAN, RIDDLER, 'ally': DICK GRAYSON, RICHARD GRAYSON, BARBARA GORDON, DIANA PRINCE}
 
 favorite_hero = {
-    "BATMAN": "Wow. Yeah! All right!\nNow it's YOUR turn for Q&A!",
-    "YOU": "Wow. Yeah! All right!\nNow it's YOUR turn for Q&A!",
-    "WONDER WOMAN": "Yeah! Mine too! Ahem, I mean: On to Q&A!",
-    "WONDERWOMAN": "Yeah! Mine too! Ahem, I mean: On to Q&A!",
-    "SUPERMAN":"What?! Oh hey, lookit that - The Bat Signal!\nInterview's over!\nGotta go!",
-    "ROBIN": "\nWhat?! ...Ok. Yeah!\nMe and Robin - we make a good team!\nI'm so proud of him!\nNow it's YOUR turn for Q&A!"}
+    "BATMAN": "\n\tWow. Yeah! All right! Now it's YOUR turn for Q&A!",
+    "YOU": "\n\tWow. Yeah! All right! Now it's YOUR turn for Q&A!",
+    "WONDER WOMAN": "\n\tYeah! Mine too! Ahem, I mean: On to Q&A!",
+    "WONDERWOMAN": "\n\tYeah! Mine too! Ahem, I mean: On to Q&A!",
+    "SUPERMAN": "\n\tWhat?! Oh hey, lookit that - The Bat Signal!\n\tInterview's over! Gotta go!",
+    "ROBIN": "\n\tYeah! Me and Robin - we make a good team!\n\tI'm so proud of him!\n\tNow it's YOUR turn for Q&A!\n\t"
+    }
 
+batman_response = {
+    "weapon": "\n\tBatmobile, because my current model can turn into a Monster Truck!",
+    "wisdom": "\n\tBe Cool. Always stand up for what's right.\n\tTreat your friends like the super heroes they are!"
+    }
+
+#More weapons and wisdom="Batarang, because it's shaped like a bat!", "\n\tI am MEGA Cool.\n\tThis Coolness didn't happen overnight.\n\tIt takes time and practice to build your cool superpower.\n\tAnd Great Things are worth the wait!"
 #superhero_keys = {"BATMAN": "Batman", "YOU": "Batman"}
 #favorite_hero=raw_input('whosyourfavorite hero').upper()
 #hero_key = superhero_keys[favorite_hero]
 #print superhero[hero_key]
 
-# def start_background_check():
-        
-    #"""Get guest background info"""
-
-#     guest_name = raw_input("What's your first name? >>> ")
-
-#     guest_name = guest_name.upper()
-
-#     if guest_name == "JOKER":
-        
-#         print "Ah ha! My arch nemesis and Frenemy. Alright, I'll give you a minute."
-
-#         guest_lives_here()
-    
-#     elif guest_name != "JOKER":
-
-#         print "All right {}. Hmmm. I once battled a villian with that *same* name. Caught 'em on the FIRST TRY!".format(guest_name.upper())
-
-#         my_guest_list.append
-        
-#         guest_lives_here()
-
-#     else:
-
-#         print "Hmm. Not answering sounds pretty suspicious. Computer, home in on the location!"
-
 
 def start_background_check():
-        
     """Get guest background info"""
 
-    guest_name = raw_input("What's your first name? >>> ")
+    guest_name = raw_input("\nWhat's your first name? ")
 
     guest_name = guest_name.upper()
 
     if guest_name == "JOKER":
         
         print """
-        \n\tAh ha! My arch nemesis and Frenemy.
-        \n\tWhat evil trick do you have up your sleeve now?
+        \n\tAh ha! JOKER!! You are my arch nemesis and Frenemy.
+        \n\tWhat evil trick do you have up your sleeve now? Ready to battle?
         """
     
     elif guest_name != "JOKER":
 
-        print "\n\t{}... Hmmm. I once battled a villian with that *same* name. \n\tCaught 'em on the FIRST TRY! \n\tOK Next question while Computer traces this call...\n\t".format(guest_name.upper())
+        print "\n\t{}... Hmmm. I once battled a villian with that *same* name.\n\tCaught 'em on the FIRST TRY!\n\tOK Next question while Computer traces this call...\n\t".format(guest_name.upper())
 
         my_guest_list.append
         
@@ -113,14 +134,14 @@ def start_background_check():
 def guest_lives_here():
     """Find out guest's hometown"""
 
-    guest_hometown = raw_input(" Where are you from?>>> ")
+    guest_hometown = raw_input(" Where are you from? ")
 
     guest_hometown = guest_hometown.title()
     
     print """
-    \nHuh. {}. What a coincidence. I have (ahem)...
-    \nI MEAN, I HEARD that Uhhh.. WAYNE ENTERPRISES owns most of the land out there...
-    \n...Interesting. OK next question!
+    \n\tHuh. {}. What a coincidence. I have (ahem)...
+    \n\tI MEAN, I HEARD that Uhhh.. WAYNE ENTERPRISES owns most of the land out there...
+    \n\t...Interesting. OK next question!
     """.format(guest_hometown)
 
     guest_favorite_superhero()
@@ -128,9 +149,9 @@ def guest_lives_here():
 
 def guest_favorite_superhero():
 
-    """Who is our guest's favorite superhero?"""
+    """Ask for guest's favorite superhero"""
 
-    favorite_superhero = raw_input("So...Who is your favorite super hero? Of all time?>>> ")
+    favorite_superhero = raw_input("So...Who is your favorite super hero? Of all time? ")
     #Create random list of Batman asking this question in a few different ways. (?)
 
     favorite_superhero = favorite_superhero.upper()
@@ -145,7 +166,6 @@ def guest_favorite_superhero():
     elif favorite_superhero == "WONDER WOMAN" or "WONDERWOMAN":
         
         print favorite_hero["WONDER WOMAN"]
-
         #print "Yeah! Mine too! Ahem, I mean: On to Q&A!"
         
         ask_batman()
@@ -160,42 +180,58 @@ def guest_favorite_superhero():
     elif favorite_superhero == "ROBIN" or "BOY WONDER":
 
         print favorite_hero["ROBIN"]
-
         #print "\nWhat?! ...Ok. Yeah!\nMe and Robin - we make a good team!\nI'm so proud of him!\nNow it's YOUR turn for Q&A!"
 
         ask_batman()
 
     else:
+        
         print "Wait, now.\nWho??\nThose are NOT even superheroes!\nName someone else!"
 #       Ask favorite super hero question again - random question)
-            
+
         guest_favorite_superhero()
 
-    return favorite_superhero
+    # return favorite_superhero
 
 
 def ask_batman():
 
     print """"
-    /nOK. Now your turn.
+    \nOK. Now your turn.
     \nWe have one minute til my Lobster Thermidor is ready to eat!
-    \nWhat do you want to know about me:
+    \nWhat do you want to know about me?
+    \n
     \n\tA - My favorite Bat weapon
-    \n\tB - My greatest Feat
-    \n\tC - Random bit of Bat Wisdom
+    \n\tB - My greatest Fears
+    \n\tC - Random bits of Bat Wisdom
     """
-    
-    batman_question = raw_input("Choose A, B or C: ")
-    batman_question = batman_question.upper
 
-    pass
+    batman_question = raw_input("Choose A, B or C: ")
+    
+    batman_question = batman_question.upper()
+
+    if batman_question == "A":
+
+        print "\n\tHa! Easy question!\n\tAnything with a Bat shape on it!\n\tRight now I'm working on jazzing up my", batman_response["weapon"]
+
+    elif batman_question == "B":
+
+        print "\n\tYeah, that's a tough one.\n\tIn my last epic movie, I learned that didn't have to fight the bad guys all by myself.\n\tI have friends.\n\tThat scared me."
+
+    elif batman_question == "C":
+
+        print "\n\tHere's my favorite:", batman_response["wisdom"]
+
+    else:
+
+        catch_suspicious_person()
 
 
 def theme_song():
 
     """print theme song string. Exit."""
 
-    print "\n\tNa-nuh Na-nuh!" * 4 + "\t***Batman!***\t*** Ka Pow! ***"
+    print "\n\tNa-nuh Na-nuh!" * 4 + "\n\t***Batman!***\n\t*** Ka Pow! ***\n\n"
 
 
 def catch_suspicious_person():
@@ -204,6 +240,24 @@ def catch_suspicious_person():
     print "\n\tHmmm. Not answering is super suspicious.\n\tI'm going to bring you in for questioning.\n\tComputer has a lock on your location.\n\tBetter run! Gotham Police will be there is 30 seconds!\n\t"
 
     print theme_song()
+
+
+def Bye():
+    """Final goodbye and open turtle to close"""
+
+    # print """
+    #     # \n\tThanks for chatting! One last word of wisdom!
+    #     # \n\tYou know MY logo. It's sleek and chic and TRADEMARKED,
+    #     # \n\tand never goes out of style!
+    #     # \n\tWhat's your cool symbol?
+    #     # \n\tNow it's your turn to draw your own symbol!
+    #     \n\tThanks for playing!
+    #     """
+    
+    print ""
+    
+    print "\n\tThanks for playing!\n\t"
+    # draw()
 
 
 def lego_batman_chat_start():
@@ -221,8 +275,8 @@ def lego_batman_chat_start():
     \tto make sure you're not on my wanted list.
     \tTell the truth, or get booted!
     \n\tReady?
-    \n\tType "Y" to start.
-    \tIf you wanna skip this part and stay anonymous, type "N"
+    \n\tType 'Y' to start.
+    \tIf you wanna skip this part and stay anonymous, type 'N'
     \tIf you've got something to hide, start running now!!
     """
 
@@ -232,15 +286,15 @@ def lego_batman_chat_start():
 
     if start_choice >= "Y":
 
-        print "All right! Let's get started."
+        print "\nAll right! Let's get started."
 
         start_background_check()
 
     elif start_choice >= "N":
 
         print """
-        \nHmmm...Sounds sneaky.
-        \nLooks like you've got something to hide.
+        \nHmmm...Not answering makes it seem like
+        \nyou've got something to hide.
         \nI'm gonna go eat my Lobster Thermidor now.
         \nGood bye!
         """
@@ -251,16 +305,17 @@ def lego_batman_chat_start():
 
     else:
         #This person is suspicious!
-
         print """
         \n\tHmmm. Not answering is super suspicious.
         \n\tWe'd better bring you in for questioning.
         \n\tComputer has traced your location.
-        \n\tBetter run! Gotham Police will be there is 30 seconds!"\n\t
+        \n\tBetter run! Gotham Police will be there is 30 seconds!
         """
-
         print theme_song()
 
-    print "Thanks for playing!"
+    #turtle.mainloop()
+
+    Bye()    
+
 #-----------------------------------------------
 lego_batman_chat_start()
